0d84beac0fe848adff76a9f1ff85550a5d345ae2
M-Attila <menyhart.atika@gmail.com>
2020-10-26 11:19:32 +0100

    Login complete

--- main.js
+++ main.js
@@ -21,30 +21,55 @@ let notesTest = [
     date: "4/1/2020",
   },
 ];
-
-notesTest.sort((a, b) => {
-  let date1 = new Date(a.date).getTime();
-  let date2 = new Date(b.date).getTime();
-  return date1 - date2;
-});
-localStorage.setItem("password", "1234");
+// localStorage.setItem("notes", JSON.stringify(notesTest));
 
 let notes = JSON.parse(localStorage.getItem("notes"));
 let password = localStorage.getItem("password");
 
+notes.sort((a, b) => {
+  let date1 = new Date(a.date).getTime();
+  let date2 = new Date(b.date).getTime();
+  return date1 - date2;
+});
+
+function setSetting(setting, value) {
+  switch (setting) {
+    case "password":
+      password = value;
+      break;
+    case "notes":
+      notes = value;
+      break;
+  }
+  localStorage.setItem(setting, value);
+}
+
+function loadLogin() {
+  if (password == null) {
+    document.getElementById("login").classList.add("no_account");
+    console.log("no password");
+  }
+}
+
 function login() {
   if (document.getElementById("passwordInput").value == password) {
     loadSite();
-  }
-  else {
-    console.log("Helytelen jelszó!");
+  } else if (password == null) {
+    setSetting("password", document.getElementById("newPasswordInput").value);
+    loadSite();
   }
 }
 
 function loadSite() {
   let siteContent = "";
-  notesTest.forEach(i => {
-    siteContent += `<div>${i.title}: ${i.content} - on ${i.date}</div>`
-  });
+  if (notes == null) siteContent = "No notes yet";
+  else {
+    notes.forEach((i) => {
+      // let currentDate = new Date(i.date);
+      // siteContent += `<div>${i.title}: ${i.content} - on ${currentDate.getFullYear()}. ${currentDate.getMonth()}. ${currentDate.getDay()}.</div>`
+      siteContent += `<div>${i.title}: ${i.content} - on ${i.date}</div>`;
+    });
+  }
+  document.getElementById("login").classList.add("hide");
   document.getElementById("content").innerHTML = siteContent;
 }
