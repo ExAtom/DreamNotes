8d5885451e9b35b4a0899031cf5d109327bbe9df
M-Attila <menyhart.atika@gmail.com>
2020-10-28 18:54:53 +0100

    Adding new notes is now possible

--- main.js
+++ main.js
@@ -1,47 +1,28 @@
-let notesTest = [
-  {
-    title: "first",
-    content:
-      "Now I have to write something here, but idk what. I think this is enough.",
-    date: "1/1/2020",
-  },
-  {
-    title: "fourth",
-    content: "asd",
-    date: "12/31/2020",
-  },
-  {
-    title: "second",
-    content: "asd",
-    date: "2/31/2020",
-  },
-  {
-    title: "third",
-    content: "asd",
-    date: "4/1/2020",
-  },
-];
-// localStorage.setItem("notes", JSON.stringify(notesTest));
-
 let notes = JSON.parse(localStorage.getItem("notes"));
+if (notes == null) notes = [];
 let password = localStorage.getItem("password");
+sortNotes();
 
-notes.sort((a, b) => {
-  let date1 = new Date(a.date).getTime();
-  let date2 = new Date(b.date).getTime();
-  return date1 - date2;
-});
+function sortNotes() {
+  notes.sort((a, b) => {
+    return b.date - a.date;
+  });
+}
+
+console.log(notes);
 
 function setSetting(setting, value) {
   switch (setting) {
     case "password":
       password = value;
+      localStorage.setItem(setting, value);
       break;
     case "notes":
-      notes = value;
+      notes[notes.length] = value;
+      localStorage.setItem("notes", JSON.stringify(notes));
+      sortNotes();
       break;
   }
-  localStorage.setItem(setting, value);
 }
 
 function toggleClass(id, className) {
@@ -49,32 +30,54 @@ function toggleClass(id, className) {
 }
 
 function loadLogin() {
-  if (password == null) toggleClass("login", "no_account");
+  if (password == null) toggleClass("body", "no-account");
 }
 
 function login() {
   if (document.getElementById("passwordInput").value == password) {
-    loadSite();
+    loadSite(true);
   } else if (password == null) {
     setSetting("password", document.getElementById("newPasswordInput").value);
-    loadSite();
+    loadSite(true);
   }
 }
 
-function loadSite() {
+function loadSite(loginVisible) {
   let siteContent = "";
-  if (notes == null) siteContent = "No notes yet";
+  if (notes.length == 0) siteContent = "No notes yet";
   else {
     notes.forEach((i) => {
       // let currentDate = new Date(i.date);
-      // siteContent += `<div>${i.title}: ${i.content} - on ${currentDate.getFullYear()}. ${currentDate.getMonth()}. ${currentDate.getDay()}.</div>`
-      siteContent += `<div>${i.title}: ${i.content} - on ${i.date}</div>`;
+      // siteContent += `<div>${i.title}: ${i.content} - on${currentDate.getFullYear()}. ${currentDate.getMonth()}. ${currentDate.getDay()}.</div>`
+      let d = new Date(i.date);
+      siteContent += `<div>${i.title}: ${i.content} - ${d.getMonth()}/${d.getDate()}/${d.getFullYear()}, ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}</div>`;
     });
   }
-  toggleClass("body", "hide-login");
+  if (loginVisible) toggleClass("body", "hide-login");
   document.getElementById("notes").innerHTML = siteContent;
 }
 
 function addNote() {
+  document.getElementById("note").value = "";
+  document.getElementById("title").value = "";
   toggleClass("body", "show-editor");
 }
+
+function closeNote() {
+  let noteTitle = document.getElementById("title").value;
+  let note = document.getElementById("note").value;
+  let d = new Date();
+  console.log("Title: " + title);
+  console.log("Note: " + note);
+  if (noteTitle != "" && note != "") {
+    setSetting("notes", {
+      title: noteTitle,
+      content: note,
+      date: d.getTime()
+    });
+  }
+  toggleClass("body", "show-editor");
+  loadSite(false);
+}
+
+function openNote(title) {}
